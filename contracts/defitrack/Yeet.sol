a87bc9f33c56df0e718d2d067cc4c0c987518d4d
QuintenDes <quintendeswaef@gmail.com>
2021-11-04 12:36:00 +0100

    Basic Yeet Contract

---
+++ contracts/defitrack/Yeet.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+abstract contract Yeet is Ownable {
+    using SafeERC20 for IERC20;
+    bool public stopped = false;
+
+    uint256 public goodwill;
+    mapping(address => bool) public approvedTargets;
+
+    address internal constant ETHAddress =
+    0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    constructor(uint256 _goodwill) {
+        goodwill = _goodwill;
+    }
+
+    // circuit breaker modifiers
+    modifier stopInEmergency {
+        if (stopped) {
+            revert("Temporarily Paused");
+        } else {
+            _;
+        }
+    }
+
+    function _getBalance(address token)
+    internal
+    view
+    returns (uint256 balance)
+    {
+        if (token == address(0)) {
+            balance = address(this).balance;
+        } else {
+            balance = IERC20(token).balanceOf(address(this));
+        }
+    }
+
+    function _approveToken(address token, address spender) internal {
+        IERC20 _token = IERC20(token);
+        if (_token.allowance(address(this), spender) > 0) return;
+        else {
+            _token.safeApprove(spender, type(uint256).max);
+        }
+    }
+
+    function _approveToken(
+        address token,
+        address spender,
+        uint256 amount
+    ) internal {
+        IERC20(token).safeApprove(spender, 0);
+        IERC20(token).safeApprove(spender, amount);
+    }
+
+    // - to Pause the contract
+    function toggleContractActive() public onlyOwner {
+        stopped = !stopped;
+    }
+
+    function set_new_goodwill(uint256 _new_goodwill) public onlyOwner {
+        require(
+            _new_goodwill >= 0 && _new_goodwill <= 100,
+            "GoodWill Value not allowed"
+        );
+        goodwill = _new_goodwill;
+    }
+
+
+
+    ///@notice Withdraw goodwill share, retaining affilliate share
+    function withdrawTokens(address[] calldata tokens) external onlyOwner {
+        for (uint256 i = 0; i < tokens.length; i++) {
+            uint256 qty;
+
+            if (tokens[i] == ETHAddress) {
+                qty = address(this).balance;
+                Address.sendValue(payable(owner()), qty);
+            } else {
+                qty =
+                IERC20(tokens[i]).balanceOf(address(this));
+                IERC20(tokens[i]).safeTransfer(owner(), qty);
+            }
+        }
+    }
+
+    function setApprovedTargets(
+        address[] calldata targets,
+        bool[] calldata isApproved
+    ) external onlyOwner {
+        require(targets.length == isApproved.length, "Invalid Input length");
+
+        for (uint256 i = 0; i < targets.length; i++) {
+            approvedTargets[targets[i]] = isApproved[i];
+        }
+    }
+
+    receive() external payable {
+        require(msg.sender != tx.origin, "Do not send ETH directly");
+    }
+}
