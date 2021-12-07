c9c06bad937a0fe359fa206e62eeafbde5bea7f7
QuintenDes <quintendeswaef@gmail.com>
2021-12-07 15:58:41 +0100

    add max calculation for distribution

--- contracts/humandao/merkle/MerkleDistributor.sol
+++ contracts/humandao/merkle/MerkleDistributor.sol
@@ -1,10 +1,15 @@
 import "./IMerkleDistributor.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract MerkleDistributor is IMerkleDistributor,  Pausable, Ownable {
+
+    using SafeERC20 for IERC20;
+
 
     address public immutable override token;
     bytes32 public immutable override merkleRoot;
@@ -32,7 +37,9 @@ contract MerkleDistributor is IMerkleDistributor,  Pausable, Ownable {
         claimedBitMap[claimedWordIndex] = claimedBitMap[claimedWordIndex] | (1 << claimedBitIndex);
     }
 
-    function doClaim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) internal override {
+    function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) external override {
+
+
         require(!isClaimed(index), 'MerkleDistributor: Drop already claimed.');
 
         // Verify the merkle proof.
@@ -41,8 +48,24 @@ contract MerkleDistributor is IMerkleDistributor,  Pausable, Ownable {
 
         // Mark it claimed and send the token.
         _setClaimed(index);
-        require(IERC20(token).transfer(account, amount), 'MerkleDistributor: Transfer failed.');
 
-        emit Claimed(index, account, amount);
+        uint256 distributionAmount = calculateMaxDistribution(account, amount);
+
+        require(IERC20(token).transfer(account, distributionAmount), 'MerkleDistributor: Transfer failed.');
+
+        emit Claimed(index, account, distributionAmount);
+    }
+
+    //Returns the amount a user would receive. Either 20% of the user's current balance or the distribution amount
+    //whichever is the lowest.
+    function calculateMaxDistribution(address account_, uint256 amount_) public view returns (uint256){
+        IERC20 erc20 = IERC20(token);
+        uint256 currentBalance = erc20.balanceOf(account_);
+        uint256 bonusOnCurrentBalance = currentBalance / 5;
+        if(bonusOnCurrentBalance >= amount_) {
+            return amount_;
+        } else {
+            return bonusOnCurrentBalance;
+        }
     }
 }
